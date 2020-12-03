dfe9b45eedf631aabd3f08e4c138a33f57fcf3b3
Ilya <ilia.yershov@gmail.com>
2020-12-03 15:38:52 +0100

    adding test Terms page

--- src/components/Terms.tsx
+++ src/components/Terms.tsx
@@ -4,14 +4,59 @@ import './../../src/css/index.css';
 // import { GridList, Checkbox, FormControlLabel, TextField } from '@material-ui/core';
 // import { themeStyles } from '../style/postcss';
 // import { BorderColor } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    position: 'relative',
+    minWidth: '1600px',
+    overflowX: 'auto',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  contentHolder: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+  },
+  tableHolder: {
+    flexGrow: 1,
+  },
+  asideHolder: {
+    width: '180px',
+    paddingLeft: '20px',
+  },
+  topHolder: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+  },
+  logoHolder: {
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  asideTopHolder: {
+    paddingLeft: '20px',
+  },
+}));
 
 export const Terms = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <div className="topadv">
-        <p>Please disable your ad-blocker (adv 728*90 align=right)</p>
-        <p>You may not like this ad, but it supports the developer and keeps this site free.</p>
-      </div>
+    <div className={classes.root}>
+      
+        <p><strong>Terms of agreement</strong></p>
+
+        <p>Agreement to use the services of TradePump.com.
+        This agreement, hereinafter referred to as the "Agreement", is concluded between the TradePump.com service, hereinafter referred to as the "Site", 
+        and the user of the services, hereinafter referred to as the "Client" or "User", determining the conditions for purchasing services through the Site.
+        </p>
+      
     </div>
   );
 };
