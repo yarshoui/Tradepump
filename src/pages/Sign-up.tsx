bfedb046a56e721fcc321e90e7713e53a056c5a2
Ilya <ilia.yershov@gmail.com>
2021-05-24 14:47:17 +0200

    Adding Trades History form

--- src/pages/Sign-up.tsx
+++ src/pages/Sign-up.tsx
@@ -102,6 +102,13 @@ const validate = (values:any) => {
   return errors;
 };
 
+function countryToFlag(isoCode: string) {
+  return typeof String.fromCodePoint !== 'undefined'
+    ? isoCode
+        .toUpperCase()
+        .replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + 127397))
+    : isoCode;
+}
 
 
 ReactGA.initialize('UA-184831310-1');
